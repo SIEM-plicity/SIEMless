b68266045aae94d3b1cbe00133b3275e7713def3
Sanai Meles <smeles.if25@theknowledgehouse.org>
2025-10-06 17:08:09 -0400

    feat(iac): add VPC + IAM baseline (v1)

---
+++ iac/main.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = var.region
+  profile = var.aws_profile
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name        = "${var.project}-vpc"
+    Environment = var.environment
+  }
+}
+
+resource "aws_subnet" "private_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_cidr
+  availability_zone = var.az_1
+
+  tags = {
+    Name = "${var.project}-private-1"
+  }
+}
+
+output "vpc_id" {
+  description = "The VPC id created for this environment"
+  value       = aws_vpc.main.id
+}
+
